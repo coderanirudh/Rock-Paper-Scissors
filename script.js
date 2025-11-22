3c0d854d7028e33faf31cac6324b57c71b99646c
Anirudh Vankayala <152790143+coderanirudh@users.noreply.github.com>
2025-11-22 22:09:41 +0530

    Create a function playGame and move playRound and for loop into it

--- script.js
+++ script.js
@@ -19,40 +19,44 @@ function getHumanChoice() {
     let humanChoice = prompt("Enter your choice between (Rock / Paper / Scissors) to play: ");
 }
 
-function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
+function playGame() {
+    function playRound(humanChoice, computerChoice) {
+        humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
 
-    console.log("Human Choice: ", humanChoice);
-    console.log("Computer Choice: ", computerChoice);
+        console.log("Human Choice: ", humanChoice);
+        console.log("Computer Choice: ", computerChoice);
 
-    if (humanChoice === computerChoice) {
-        console.log("It's a tie!");
-    } else if (
-        (humanChoice === "Rock" && computerChoice === "Scissors") ||
-        (humanChoice === "Paper" && computerChoice === "Rock") ||
-        (humanChoice === "Scissors" && computerChoice === "Paper")
-    ) {
-        console.log("You win!");
-        humanScore++;
-    } else if (
-        (humanChoice === "Rock" && computerChoice === "Paper") ||
-        (humanChoice === "Paper" && computerChoice === "Scissors") ||
-        (humanChoice === "Scissors" && computerChoice === "Rock")
-    ) {
-        console.log("You lose!");
-        computerScore++;
-    } else {
-        console.log("Invalid input");
+        if (humanChoice === computerChoice) {
+            console.log("It's a tie!");
+        } else if (
+            (humanChoice === "Rock" && computerChoice === "Scissors") ||
+            (humanChoice === "Paper" && computerChoice === "Rock") ||
+            (humanChoice === "Scissors" && computerChoice === "Paper")
+        ) {
+            console.log("You win!");
+            humanScore++;
+        } else if (
+            (humanChoice === "Rock" && computerChoice === "Paper") ||
+            (humanChoice === "Paper" && computerChoice === "Scissors") ||
+            (humanChoice === "Scissors" && computerChoice === "Rock")
+        ) {
+            console.log("You lose!");
+            computerScore++;
+        } else {
+            console.log("Invalid input");
+        }
+    }
+
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        const validRound = playRound(humanSelection, computerSelection);
+        if (!validRound) {
+            console.log("Enter a valid input!");
+            i--;
+        }
     }
 }
 
-for (let i = 0; i < 5; i++) {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    const validRound = playRound(humanSelection, computerSelection);
-    if (!validRound) {
-        console.log("Enter a valid input!");
-        i--;
-    }
-}
+
 
